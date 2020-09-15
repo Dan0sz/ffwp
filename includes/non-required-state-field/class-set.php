5c18836d1d30aa12ff4c005bd988fc33be7bdc92
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2020-09-16 01:49:56 +0200

    Required state is now dynamically set, based on a list of countries where state is required.

--- includes/non-required-state-field/class-set.php
+++ includes/non-required-state-field/class-set.php
@@ -19,6 +19,7 @@ class FFWP_NonRequiredStateField_Set
     public function __construct()
     {
         add_filter('edd_purchase_form_required_fields', [$this, 'remove_state_from_required_fields']);
+        add_action('edd_cc_billing_bottom', [$this, 'insert_script']);
     }
 
     /**
@@ -33,5 +34,39 @@ class FFWP_NonRequiredStateField_Set
         }
 
         return $fields;
+    }
+
+    public function insert_script()
+    {
+        ?>
+        <script>
+            jQuery(document).ready(function ($) {
+                var ffwp = {
+                    $card_state_label: $('#edd-card-state-wrap label'),
+                    required_countries: [
+                        'US', 'AO', 'CA', 'AU', 'BD', 'BG', 'BR', 'CN', 'GB', 'HK', 'HU', 'ID', 'IN', 'IR', 'IT', 'JP', 'MX', 'MY', 'NP', 'NZ', 'PE', 'TH', 'TR', 'ZA', 'ES'
+                    ],
+
+                    init: function () {
+                        $(document.body).on('edd_cart_billing_address_updated', this.is_required);
+                        this.is_required();
+                    },
+
+                    is_required: function () {
+                        var $billing_country = $('#billing_country').val();
+
+                        if (ffwp.required_countries.includes($billing_country)) {
+                            ffwp.$card_state_label.append('<span class="edd-required-indicator">*</span>');
+                            $('#card_state').attr('required', 'required');
+                        } else {
+                            $('#edd-card-state-wrap label .edd-required-indicator').remove();
+                        }
+                    }
+                };
+
+                ffwp.init();
+            });
+        </script>
+        <?php
     }
 }
