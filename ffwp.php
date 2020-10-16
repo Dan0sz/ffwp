c99157778262ce81e8bcac317720d297a0962ffe
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2020-10-16 08:53:28 +0200

    v1.5.4

--- ffwp.php
+++ ffwp.php
@@ -3,7 +3,7 @@
  * @formatter:off
  * Plugin Name: Easy Digital Downloads - FFWP Modifications
  * Description: Custom additions to EDD for Fast FW Press.
- * Version: 1.5.3
+ * Version: 1.5.4
  * Author: Daan (from Fast FW Press)
  * Author URI: https://ffwp.dev
  * Text Domain: ffwp
