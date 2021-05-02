87674a3c56965ad2f56b951b4594838de61cb7ec
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2021-05-02 17:10:45 +0000

    v2.0.4

--- ffwp.php
+++ ffwp.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Easy Digital Downloads - FFWP Modifications
  * Description: Custom additions to EDD for Fast FW Press.
- * Version: 2.0.3
+ * Version: 2.0.4
  * Author: Daan (from FFW.Press)
  * Author URI: https://ffw.press
  * Text Domain: ffwp
@@ -16,7 +16,7 @@
 define('FFWP_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('FFWP_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('FFWP_PLUGIN_FILE', __FILE__);
-define('FFWP_STATIC_VERSION', '2.0.3');
+define('FFWP_STATIC_VERSION', '2.0.4');
 
 /**
  * Takes care of loading classes on demand.
