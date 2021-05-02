0e3231f4ed5d8dca573f3da533901f5d60fd6f21
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2021-05-02 17:39:06 +0000

    Fixed padding for logged in users.

--- includes/login-fields-legend/class-insert.php
+++ includes/login-fields-legend/class-insert.php
@@ -54,6 +54,7 @@ class FFWP_LoginFieldsLegend_Insert
             }
 
             #ffwp-account-form p {
+                padding-top: 0 im !important;
                 padding-bottom: 0 !important;
                 padding-left: 30px;
                 padding-right: 30px;
