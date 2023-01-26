e39a1184ab7912d140a4b7611d1079db1711f7d0
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-01-26 22:43:34 +0000

    Improved: move tax row up so it makes sense.

--- includes/better-checkout/edd_templates/checkout_cart.php
+++ includes/better-checkout/edd_templates/checkout_cart.php
@@ -85,6 +85,25 @@ global $post; ?>
 			</tr>
 		<?php endif; ?>
 
+		<tr class="edd_cart_footer_row edd_cart_discount_row" <?php if (!edd_cart_has_discounts())  echo ' style="display:none;"'; ?>>
+			<?php do_action('edd_checkout_table_discount_first'); ?>
+			<th colspan="<?php echo edd_checkout_cart_columns(); ?>" class="edd_cart_discount">
+				<?php edd_cart_discounts_html(); ?>
+			</th>
+			<?php do_action('edd_checkout_table_discount_last'); ?>
+		</tr>
+
+		<?php if (edd_use_taxes()) : ?>
+			<tr class="edd_cart_footer_row edd_cart_tax_row" <?php if (!edd_is_cart_taxed()) echo ' style="display:none;"'; ?>>
+				<?php do_action('edd_checkout_table_tax_first'); ?>
+				<th colspan="<?php echo edd_checkout_cart_columns(); ?>" class="edd_cart_tax">
+					<?php _e('Tax', 'easy-digital-downloads'); ?>:&nbsp;<span class="edd_cart_tax_amount" data-tax="<?php echo edd_get_cart_tax(false); ?>"><?php echo esc_html(edd_cart_tax()); ?></span>
+				</th>
+				<?php do_action('edd_checkout_table_tax_last'); ?>
+			</tr>
+
+		<?php endif; ?>
+
 		<!-- Show any cart fees, both positive and negative fees -->
 		<?php if (edd_cart_has_fees()) : ?>
 			<?php foreach (edd_get_cart_fees() as $fee_id => $fee) : ?>
@@ -107,24 +126,6 @@ global $post; ?>
 				</tr>
 			<?php endforeach; ?>
 		<?php endif; ?>
-
-		<?php if (edd_use_taxes()) : ?>
-			<tr class="edd_cart_footer_row edd_cart_tax_row" <?php if (!edd_is_cart_taxed()) echo ' style="display:none;"'; ?>>
-				<?php do_action('edd_checkout_table_tax_first'); ?>
-				<th colspan="<?php echo edd_checkout_cart_columns(); ?>" class="edd_cart_tax">
-					<?php _e('Tax', 'easy-digital-downloads'); ?>:&nbsp;<span class="edd_cart_tax_amount" data-tax="<?php echo edd_get_cart_tax(false); ?>"><?php echo esc_html(edd_cart_tax()); ?></span>
-				</th>
-				<?php do_action('edd_checkout_table_tax_last'); ?>
-			</tr>
-		<?php endif; ?>
-
-		<tr class="edd_cart_footer_row edd_cart_discount_row" <?php if (!edd_cart_has_discounts())  echo ' style="display:none;"'; ?>>
-			<?php do_action('edd_checkout_table_discount_first'); ?>
-			<th colspan="<?php echo edd_checkout_cart_columns(); ?>" class="edd_cart_discount">
-				<?php edd_cart_discounts_html(); ?>
-			</th>
-			<?php do_action('edd_checkout_table_discount_last'); ?>
-		</tr>
 
 		<tr class="edd_cart_footer_row">
 			<?php do_action('edd_checkout_table_footer_first'); ?>
