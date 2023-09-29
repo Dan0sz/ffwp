5311ce44ba16cb1882bc332fe356542a0ee54a7b
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-09-29 21:40:36 +0200

    Fixed: prevent warning on get_headers()

--- includes/download-info/class-shortcodes.php
+++ includes/download-info/class-shortcodes.php
@@ -10,56 +10,57 @@
  *            http://creativecommons.org/licenses/by-nc-nd/4.0/
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-class FFWP_DownloadInfo_Shortcodes
-{
-    public function __construct()
-    {
-        add_shortcode('edd_dl_version', [$this, 'get_current_version']);
-        add_shortcode('edd_dl_last_updated', [$this, 'get_date_last_updated']);
-    }
-
-    /**
-     * Shortcode: EDD Download Version
-     * 
-     * [edd_dl_version id="post_id"]
-     * 
-     * "id": defaults to current post.
-     */
-    public function get_current_version($attributes)
-    {
-        $attributes = shortcode_atts([
-            'id' => get_the_ID(),
-        ], $attributes);
-
-        return get_post_meta($attributes['id'], '_edd_sl_version', true) ?? '';
-    }
-
-    /**
-     * Shortcode: EDD Product Last Updated
-     * 
-     * [edd_dl_last_updated id="post_id" format="date_format"]
-     * 
-     * "id": defaults to current post
-     * "date_format": defaults to date_format option from wp_options table. 
-     */
-    public function get_date_last_updated($attributes)
-    {
-        $attributes = shortcode_atts([
-            'id' => get_the_ID(),
-            'format' => get_option('date_format')
-        ], $attributes);
-
-        $readme_url = get_post_meta($attributes['id'], '_edd_readme_location', true) ?? '';
-        $headers    = get_headers($readme_url);
-
-        foreach ($headers as $header) {
-            if (strpos($header, 'Last-Modified') !== false) {
-                $timestamp = strtotime(str_replace('Last-Modified: ', '', $header));
-
-                return gmdate($attributes['format'], $timestamp);
-            }
-        }
-    }
+class FFWP_DownloadInfo_Shortcodes {
+	public function __construct() {
+		add_shortcode( 'edd_dl_version', [ $this, 'get_current_version' ] );
+		add_shortcode( 'edd_dl_last_updated', [ $this, 'get_date_last_updated' ] );
+	}
+	
+	/**
+	 * Shortcode: EDD Download Version
+	 *
+	 * [edd_dl_version id="post_id"]
+	 *
+	 * "id": defaults to current post.
+	 */
+	public function get_current_version( $attributes ) {
+		$attributes = shortcode_atts( [
+			                              'id' => get_the_ID(),
+		                              ], $attributes );
+		
+		return get_post_meta( $attributes['id'], '_edd_sl_version', true ) ?? '';
+	}
+	
+	/**
+	 * Shortcode: EDD Product Last Updated
+	 *
+	 * [edd_dl_last_updated id="post_id" format="date_format"]
+	 *
+	 * "id": defaults to current post
+	 * "date_format": defaults to date_format option from wp_options table.
+	 */
+	public function get_date_last_updated( $attributes ) {
+		$attributes = shortcode_atts( [
+			                              'id' => get_the_ID(),
+			                                                                                                                              'format' => get_option( 'date_format' ),
+		                              ], $attributes );
+		
+		$readme_url = get_post_meta( $attributes['id'], '_edd_readme_location', true ) ?? '';
+		
+		if ( ! $readme_url ) {
+			return;
+		}
+		
+		$headers = get_headers( $readme_url );
+		
+		foreach ( $headers as $header ) {
+			if ( strpos( $header, 'Last-Modified' ) !== false ) {
+				$timestamp = strtotime( str_replace( 'Last-Modified: ', '', $header ) );
+				
+				return gmdate( $attributes['format'], $timestamp );
+			}
+		}
+	}
 }
